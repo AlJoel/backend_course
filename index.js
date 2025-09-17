45da498e745a15870f4bece166ce13c8b6d83ef3
joel <al.joelc@gmail.com>
2025-09-16 21:55:38 -0300

    Corrijo index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use("/turnos", turnoRoutes);
 app.use("/empleados", empleadoRoutes);
 
-// Servidor en escucha hello world
+// Servidor en escucha
 app.listen(PORT, ()=>{
   console.log(`Servidor corriendo en http://localhost:`);
 });
